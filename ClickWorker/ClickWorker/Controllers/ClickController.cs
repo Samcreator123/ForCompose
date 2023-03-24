9435c6977aee878879272c15e3edb35a4a1499a5
SYSTEX\2200531 <2200531@systex.com>
2023-03-24 15:18:06 +0800

    finish_worker

---
+++ ClickWorker/ClickWorker/Controllers/ClickController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace ClickWorker.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClickController: ControllerBase
+    {
+        [HttpPost]
+        public void Work()
+        {
+            using var connection = new SqlConnection("server=db;database=TEST;uid=sa;pwd=Sam008125;Connection Timeout=3000");
+
+            connection.Open();
+
+            var command = new SqlCommand();
+
+            command.Connection = connection;
+
+            command.CommandText = @"SELECT * FROM Click_Counter";
+
+            var reader = command.ExecuteReader();
+
+            string key = "";
+            int numBefore = 0;
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    key = reader.GetString(0);
+                    numBefore = (int)reader.GetDecimal(1);
+                    break;
+                }
+            }
+
+            reader.Close();
+
+            int numNow = numBefore + 1;
+
+            command.CommandText = @"UPDATE Click_Counter SET Number = @num WHERE CounterKey = @key";
+            command.Parameters.AddWithValue("@num", numNow);
+            command.Parameters.AddWithValue("@key", key);
+
+            command.ExecuteNonQuery();
+
+            connection.Close();
+
+        }
+    }
+}
