ddbba43bf36082dc09aa85fa3578d24af3877eeb
SYSTEX\2200531 <2200531@systex.com>
2023-04-02 10:46:08 +0800

    add_depends_on_v2

--- MssqlDB/entrypoint.sh
+++ MssqlDB/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo make scripts executed
+USER root
+
+RUN chmod +x /app/script/db-init.sh
+
 echo now get into entrypoint!!
 
 /opt/mssql/bin/sqlservr | /opt/mssql/bin/permissions_check.sh | /app/script/Scripts/db-init.sh
